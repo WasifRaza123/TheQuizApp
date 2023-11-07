536cd91f33880ba5edf21d85892202e2cf3870c9
Mohd Wasif Raza <wasraz86@gmail.com>
2023-11-07 06:15:39 +0530

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Mohd Wasif Raza on 07/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
